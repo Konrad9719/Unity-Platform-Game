e9fe992f64383b8495d720c8f244a0d99d5fa458
Konrad9719 <38213091+Konrad9719@users.noreply.github.com>
2018-05-20 20:30:46 +0200

    Add files via upload

---
+++ Scripts/Deaths.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Deaths : MonoBehaviour {
+    Text deaths;
+    private Player player;
+    // Use this for initialization
+    void Start()
+    {
+        deaths = GetComponent<Text>();
+        player = FindObjectOfType<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        deaths.text = "Deaths: " + player.deaths;
+    }
+}
---
+++ Scripts/Player.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+    public int movespeed;
+    public int jumppower;
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+    private bool onGround;
+    public int coins;
+    public bool moveRight;
+    public bool moveLeft;
+    public int deaths;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        movespeed = 3;
+        jumppower = 5;
+
+    }
+
+    void Update()
+    {
+        if (moveLeft || Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.velocity = new Vector2(-movespeed, rb.velocity.y);
+        }
+        if (moveRight || Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.velocity = new Vector2(movespeed, rb.velocity.y);
+        }
+        //if (Input.GetKey(KeyCode.Space) && onGround)
+        if (Input.GetKey(KeyCode.Space))
+        {
+            jump();
+        }
+    }
+
+    public void jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumppower);
+    }
+
+    void FixedUpdate()
+    {
+        onGround = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius,
+        whatIsGround);
+    }
+
+}
